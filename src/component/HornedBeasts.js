e4d7b66e671e4613e6d7cb239655b55424c05e52
awwadsaeed <saeedawwad450@gmail.com>
2021-05-17 15:49:27 +0300

    lab 02 withoute stretch goal

--- src/component/HornedBeasts.js
+++ src/component/HornedBeasts.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 class HornedBeasts extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+        votes:0,
+        }
+    }
+    setVotes=()=>{
+        this.setState({
+            votes:this.state.votes+1,
+        })
+    }
  render(){
      return(
-        <div>
-         <h2>{this.props.title}</h2>
-         <img src={this.props.src} alt={this.props.alt}/>
-         <p>{this.props.discreption}</p>
-        </div>
+        // <div>
+        //  <h2>{this.props.title}</h2>
+        //  <img src={this.props.src} alt={this.props.alt}/>
+        //  <p>{this.props.discreption}</p>
+        // <button onClick={this.setVotes}>❤️ {this.state.votes}</button>
+        // </div>
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={this.props.src} alt={this.props.title} title={this.props.title} />
+        <Card.Body>
+          <Card.Title>{this.props.title}</Card.Title>
+          <Card.Text>
+          {this.props.discreption}
+          </Card.Text>
+          <Button onClick={this.setVotes} variant="primary">❤️ {this.state.votes}</Button>
+        </Card.Body>
+      </Card>
     )
  }
 }
--- src/component/main.js
+++ src/component/main.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import HornedBeasts from './HornedBeasts';
-
-class Main extends React.Component{
-    render(){
-        return(
-            <div>
-                <HornedBeasts title={'the first horned beast'} src={'https://image.shutterstock.com/image-vector/cute-funny-bull-huge-head-260nw-158018219.jpg'} alt={'horned beast'} discreption={'funny horned beast'}/>
-                <HornedBeasts title={'the second horned beast'} src={'https://cdn5.vectorstock.com/i/1000x1000/47/99/funny-bulls-vector-44799.jpg'} alt={'horned beast'} discreption={'another funny horned beast'}/>
-            </div>
+import data from './data.json'
+import { CardColumns } from 'react-bootstrap';
+class Main extends React.Component {
+    render() {
+        return (
+            <>
+                <CardColumns>
+                    {data.map((element) => {
+                        return <HornedBeasts
+                            title={element.title}
+                            src={element.image_url}
+                            discreption={element.description}
+                        />
+                    })}
+                </CardColumns>
+            </>
+            // <div>
+            //     <HornedBeasts title={'the first horned beast'} src={'https://image.shutterstock.com/image-vector/cute-funny-bull-huge-head-260nw-158018219.jpg'} alt={'horned beast'} discreption={'funny horned beast'}/>
+            //     <HornedBeasts title={'the second horned beast'} src={'https://cdn5.vectorstock.com/i/1000x1000/47/99/funny-bulls-vector-44799.jpg'} alt={'horned beast'} discreption={'another funny horned beast'}/>
+            // </div>
         )
     }
 }
